e63947e83f48188f4b52aa240643ed6cbe8fdd4a
Paul Burt <paul.burt@gmail.com>
2018-04-12 18:37:04 +0100

    Aligned let bindings in `tv.impl/fetch-schedule!`

--- src/cljs/tv/impl.cljs
+++ src/cljs/tv/impl.cljs
@@ -107,7 +107,7 @@
   (go
     (let [{{:keys [results]} :body} (<! (fetch! id))]
       (when (seq results)
-        (let [updated (update-schedule results id)
+        (let [updated  (update-schedule results id)
               schedule (spec/conform ::schedule updated)]
           (when-not (= schedule ::spec/invalid)
             (swap! db assoc-in [:stations id :schedule] schedule)))))))
